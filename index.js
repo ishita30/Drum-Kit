9d8ada4dc17b860d094a7b78033a67738fbe3c8f
ishita30 <ishitasinghal30@gmail.com>
2021-08-10 17:51:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,48 @@ for(var i=0; i<document.querySelectorAll(".drum").length; i++)
   document.querySelectorAll(".drum")[i].addEventListener("click" , handleClick);//not added parenthesis in handleclick as then it would have called the function immediately.
   function handleClick()
   {
-    this.style.color="white";
-    // var audio = new Audio('sounds/tom-1.mp3');
-    // audio.play();
+    var buttonInnerHtml = this.innerHtml;
+    switch (buttonInnerHtml) {
+      case "w":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+      case "a":
+      var kick-bass = new Audio('sounds/kick-bass.mp3');
+      kick-bass.play();
+      break;
+
+      case "s":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+      case "d":
+      var tom-1 = new Audio('sounds/tom-1.mp3');
+      tom-1.play();
+      break;
+
+      case "j":
+      var tom-2 = new Audio('sounds/tom-2.mp3');
+      tom-2.play();
+      break;
+
+      case "k":
+      var tom-3 = new Audio('sounds/tom-3.mp3');
+      tom-3.play();
+      break;
+
+      case "l":
+      var tom-4 = new Audio('sounds/tom-4.mp3');
+      tom-4.play();
+      break;
+
+      default: console.log(buttonInnerHtml);
+
+    }
+    ////this.style.color="white";
+    //
     // alert("I got clicked")
   }
 }
