1fc0c8ef8bf0ea32319bd43ad59973b7458e41a0
ishita30 <ishitasinghal30@gmail.com>
2021-08-11 21:18:51 +0530

    Update

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
-for(var i=0; i<document.querySelectorAll(".drum").length; i++)
-{
-  document.querySelectorAll(".drum")[i].addEventListener("click" ,   function()
-  {
-    var buttonInnerHtml = this.innerHtml;
-    switch (buttonInnerHtml) {
-      case "w":
+function play() {
+  var buttonInnerhtml = this.innerHTML;
+  // console.log(buttonInnerHtml)
+  // console.log(this.innerHTML)
+  switch (buttonInnerhtml) {
+    case 'w':
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
-      case "a":
-      var kick-bass = new Audio('sounds/kick-bass.mp3');
-      kick-bass.play();
+    case 'a':
+      var kickbass = new Audio('sounds/kick-bass.mp3');
+      kickbass.play();
       break;
 
-      case "s":
+    case 's':
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
-      case "d":
+    case 'd':
       var tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
 
-      case "j":
+    case 'j':
       var tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
       break;
 
-      case "k":
+    case 'k':
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
-      case "l":
+    case 'l':
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
-      default: console.log(buttonInnerHtml);
-
-    }
-    document.addEventListener("keypress",function()
-{
-  alert("key was pressed");
-}
-);
-    ////this.style.color="white";
-    //
-    // alert("I got clicked")
+    default:
+      console.log(buttonInnerhtml);
   }
-);
-function makeSound(key)
-{
+  //     document.addEventListener('keypress', function () {
+  //       alert('key was pressed')
+  //     })
 
+  //   })
+  // }
+
+  // const check = () => {
+  //   console.log('Hi')
 }
 
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', play);
 }
